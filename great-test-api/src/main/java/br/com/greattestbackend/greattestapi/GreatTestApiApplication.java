2cdbd8caa4274953953d22c6d51bd3aaedb7bb54
caetanovns <caetanov120@gmail.com>
2024-01-19 17:42:23 -0300

    upload spring api

---
+++ great-test-api/src/main/java/br/com/greattestbackend/greattestapi/GreatTestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.greattestbackend.greattestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatTestApiApplication.class, args);
+	}
+
+}
---
+++ great-test-api/src/test/java/br/com/greattestbackend/greattestapi/GreatTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.greattestbackend.greattestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
